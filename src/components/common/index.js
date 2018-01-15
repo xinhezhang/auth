2e0a99d015aee01c402cfe3dfc86015459a0f3a5
xinhezhang <xinhezhang@hotmail.com>
2018-01-14 22:32:59 -0800

    create Spinner component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -11,3 +11,4 @@ export { default as Card } from './Card';
 export { default as CardSection } from './CardSection';
 export { default as Header } from './Header';
 export { default as Input } from './Input';
+export { default as Spinner } from './Spinner';
