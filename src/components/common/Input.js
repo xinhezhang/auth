d65a2021df185f18fe2a5a6338b4cb5627fdb97d
xinhezhang <xinhezhang@hotmail.com>
2018-01-15 01:33:50 -0800

    add more properties to Input component

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry, keyboardType, autoCapitalize }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        keyboardType={keyboardType || 'default'}
         autoCorrect={false}
+        autoCapitalize={autoCapitalize || 'none'}
         secureTextEntry={secureTextEntry}
         placeholder={placeholder}
         value={value}
