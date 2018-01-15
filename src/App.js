3c28b2d8489ecab8d81479571e79b672ebe97362
xinhezhang <xinhezhang@hotmail.com>
2018-01-15 01:09:27 -0800

    implement sign out function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      loggedIn: false,
+    });
+  }
+
   componentWillMount() {
     // initialize firebase using given configuration
     firebase.initializeApp({
@@ -15,6 +22,15 @@ export default class App extends Component {
       projectId: 'auth-react-native-xinhezhang',
       storageBucket: 'auth-react-native-xinhezhang.appspot.com',
       messagingSenderId: '222003941114'
+    });
+
+    // sign in <-> sign out
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
 
