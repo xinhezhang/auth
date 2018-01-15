0990b426ce1f4e43d293826d80acccdaab572a60
xinhezhang <xinhezhang@hotmail.com>
2018-01-14 20:40:24 -0800

    initialize firebase using given configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './components/common';
 
 export default class App extends Component {
+  componentWillMount() {
+    // initialize firebase using given configuration
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBoSUlrivmk1--pSyzlIBTuilUpChu2Qzs',
+      authDomain: 'auth-react-native-xinhezhang.firebaseapp.com',
+      databaseURL: 'https://auth-react-native-xinhezhang.firebaseio.com',
+      projectId: 'auth-react-native-xinhezhang',
+      storageBucket: 'auth-react-native-xinhezhang.appspot.com',
+      messagingSenderId: '222003941114'
+    });
+  }
+
   render() {
     return (
       <View>
